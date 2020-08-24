a255b2823f9dd8ae32472db026324ade769e04e4
Tremain Hebert <truman@Tremains-iMac.local>
2020-08-23 21:58:01 -0500

    Started Project

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AddedFeature = props => {
   return (
     <li>
-      {/* Add an onClick to run a function to remove a feature */}
+      {/* Add an onClick to run a function to remove a feature */ }
       <button className="button">X</button>
       {props.feature.name}
     </li>
