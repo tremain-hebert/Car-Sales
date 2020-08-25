db7b2d9062beafc1b1f1317605aa16e78891921c
Tremain Hebert <truman@Tremains-iMac.local>
2020-08-25 18:17:53 -0500

    MVP Completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { useReducer } from 'react';
+import { connect } from 'react-redux';
 
 import Header from './components/Header';
 import AddedFeatures from './components/AddedFeatures';
 import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 import carReducer,{ ADD_FEATURE, REMOVE_FEATURE, initialState } from './reducers/carReducer';
+import { removeFeature, addFeature } from './actions/carActions'
 
-const App = () => {
-  const [state, dispatch] = useReducer(carReducer, initialState);
+const App = (props) => {
+  // const [state, dispatch] = useReducer(carReducer, initialState);
 
-  const removeFeature = feature => {
-    dispatch({ type: REMOVE_FEATURE, payload: feature })
-  };
+  // const removeFeature = feature => {
+  //   dispatch({ type: REMOVE_FEATURE, payload: feature })
+  // };
 
-  const addFeature = feature => {
-    dispatch({ type: ADD_FEATURE, payload: feature })
-  };
+  // const addFeature = feature => {
+  //   dispatch({ type: ADD_FEATURE, payload: feature })
+  // };
 
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures removeFeature={removeFeature} car={state.car} />
+        <Header car={props.car} />
+        <AddedFeatures removeFeature={props.removeFeature} car={props.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures addFeature={addFeature} additionalFeatures={state.additionalFeatures} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures addFeature={props.addFeature} additionalFeatures={props.additionalFeatures} />
+        <Total car={props.car} additionalPrice={props.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+const mapStatetoProps = state => {
+  return state
+}
+
+const mapDispatchToProps = {
+  removeFeature,
+  addFeature
+}
+
+export default connect(mapStatetoProps, mapDispatchToProps)(App);
